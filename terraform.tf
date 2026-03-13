af50c135820a06d43665855ad21903fec3bb0b6e
harshvaidya04 <22bt04154@gsfcuniversity.ac.in>
2026-03-13 17:02:25 +0530

    Updating with S3 Bucket and EC2

--- terraform.tf
+++ terraform.tf
@@ -11,13 +11,35 @@ data "aws_ami" "ubuntu" {
   }
 
   owners = ["099720109477"] # Canonical
+# Amazon Linux : 137112412989
 }
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "learn-terraform"
+  }
+}
+# S3 Bucket using Terraform Module
+module "s3_bucket" {
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket = "harsh-terraform-s3-bucket-001"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "terraform-s3-bucket"
+    Environment = "dev"
   }
 }
